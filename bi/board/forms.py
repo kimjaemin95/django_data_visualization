f1f8e826de1c2a5fdab5d1d5542354647d90808d
jmkim <jmkim@nhsinnodep.onmicrosoft.com>
2020-09-22 13:56:58 +0900

    paginator

---
+++ bi/board/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class BoardForm(forms.Form):
+    title = forms.CharField(error_messages={
+        'required':'제목을 입력해주십시오.',
+    },max_length=128, label='제목')
+    contents = forms.CharField(error_messages={
+        'required':'내용을 입력해주십시오.'
+    },widget=forms.Textarea, label='내용')
--- bi/user/models.py
+++ bi/user/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+
 class User(models.Model):
     username = models.CharField(max_length=64, verbose_name='사용자명')
     useremail = models.EmailField(max_length=128, verbose_name='사용자이메일')
