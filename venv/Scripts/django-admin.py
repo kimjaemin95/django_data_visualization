ffe846cc6394eb5b420b33956f2a7f241fc1559f
jmkim <todaybow@naver.com>
2020-09-27 17:21:32 +0900

    kakomap

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kimjaemin\desktop\django-data-visualization\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
